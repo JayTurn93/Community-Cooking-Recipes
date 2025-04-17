a2416d0bcaaf06cc0eb3e21cc08c9018732cfcd3
JayTurn93 <jyturner93@gmail.com>
2025-04-16 21:01:10 -0400

    updated readme. add data file for recipes.

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ All pages have been converted and a Signup has been added.
 Next additions will include useState, useEffect, retrieve and store input data.
 STAY TUNED!!
 
+## Update
+--April 16, 2025 
+We've add input functionality to login and signup page (new addition). Currently working on a data for recipes. 
+Functionality to add recipes coming soon after data is created!
--- src/data/recipes.js
+++ src/data/recipes.js
@@ -1,11 +1,31 @@
 const recipes = [
     {
-        id: "",
-        name: "",
+        id: "512465",
+        name: "Steak and Egg Sandwhich",
+        prepTime: "10 minutes",
+        cookTime: "30 minutes",
+        ingredients:"Steak, 2 eggs, 2 tablespoons of butter, rolls",
+        instructions: "Prepare steak with salter and pepper. In a pan with high heat, cook steak for 3-4 minutes. Flip after 3-4 minutes. Place butter in pan and baste steak. When internal temperature reaches 136F, remove and let rest. While steak rest, fry 2 eggs. Once eggs are done, toast bread in pan for 1 minute each side. Cut steak into strips and place on on side of roll. Lay fried eggs ontop of steak. Close sandhwich with other side of roll.  ",
+        notes: ""
+
+    }
+    {
+        id: "329977",
+        name: "Cheesey Broccoli Chicken and Rice",
         prepTime: "",
         cookTime: "",
         ingredients:"",
         instructions: "",
+        notes: ""
+
+    }
+    {
+        id: "541657",
+        name: "Chicken Fried Rice",
+        prepTime: "15 minutes",
+        cookTime: "30 minutes",
+        ingredients:"leftover rice, chicken, soy sauce, vegetables of choice",
+        instructions: "Prepare chicken. Season with prefered seasoings. In a hot skillet, add ",
         notes: ""
 
     }
