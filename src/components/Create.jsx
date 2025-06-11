92ffeaad4846ac7c9202ab8671f026e1deac8b04
JayTurn93 <jyturner93@gmail.com>
2025-06-10 23:56:10 -0400

    working on create backend connection. may need to rearrange middleware to fix issue with body showing undefined for prepTime

--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -1,7 +1,10 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { Navigate, useNavigate } from "react-router-dom";
 
 function Create() {
 
+    const navigate = useNavigate()
+    const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
     const [recipeName, setRecipeName] = useState("");
     const [prepTime, setPrepTime] = useState("");
     const [cookTime, setCookTime] = useState("");
@@ -37,6 +40,18 @@ function Create() {
     const handleSubmitRecipe = (e) => {
       e.preventDefault()
       console.log("trying to",recipeName, prepTime, cookTime, ingredients, instructions, notes);
+      const body = {
+        name: e.target.name.value,
+        prepTime: e.target.prepTime.value,
+        cookTime: e.target.cookTime.value,
+        ingredients: e.target.ingredients.value,
+        instructions: e.target.instructions.value,
+        notes: e.target.notes.value
+      }
+      fetch(`${API_BASE_URL}/api/recipes/create/new`, {method: "POST"})
+      .then(JSON.stringify(body))
+      .then(navigate, ("/Home"))
+      .catch(console.error)
       
     }
 
@@ -47,7 +62,7 @@ function Create() {
             <form onSubmit={handleSubmitRecipe}>
                 <div className="recipeinput">
                     <label htmlFor="">Recipe Name: </label>
-                    <input type="text" name="" id="" placeholder="" onChange={handleRecipeNameChange}/>
+                    <input type="text" name="" id="" placeholder="" required  onChange={handleRecipeNameChange}/>
                 </div>
                 <div className="recipeinput">
                     <label htmlFor="">Prep Time: </label>
@@ -55,15 +70,15 @@ function Create() {
                 </div>
                 <div className="recipeinput">
                     <label htmlFor="recipeinput">Cook Time: </label>
-                    <input type="text" name="" id="" placeholder="" onChange={handleCookTimeChange}/>
+                    <input type="text" name="" id="" placeholder="" required onChange={handleCookTimeChange}/>
                 </div>
                 <div className="bigtextarea">
                     <label htmlFor="">Ingredients: </label>
-                    <textarea name="" id="" rows="7" cols="75" onChange={handleIngredientsChange}></textarea>
+                    <textarea name="" id="" rows="7" cols="75" required onChange={handleIngredientsChange}></textarea>
                 </div>
                 <div className="bigtextarea">
                     <label htmlFor="">Instructions: </label>
-                    <textarea name="" id="" rows="7" cols="75" onChange={handleInstructionsChange}></textarea>
+                    <textarea name="" id="" rows="7" cols="75" required onChange={handleInstructionsChange}></textarea>
                 </div>
                 <div className="bigtextarea">
                     <label htmlFor="">Notes: </label>
@@ -73,7 +88,7 @@ function Create() {
             </form>
             <div>
                 <button >Upload Photo</button>
-                <input type="file" name="" id="" />
+                <input type="file" name="" id=""  />
             </div>
         </div>
     </div>
